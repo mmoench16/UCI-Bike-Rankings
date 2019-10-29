76c1d977090658ef9b1e93ae964fa66dd7c06351
Martin Moench <martim09@nss.scot.nhs.uk>
2019-10-29 13:50:43 +0000

    Latest version of GIF

--- script.R
+++ script.R
@@ -1,8 +1,10 @@
 # Load libraries
 library(tidyverse)
+library(stringr)
 library(readxl)
 library(lubridate)
 library(gganimate)
+library(Cairo)
 
 filepath <- "C:/Users/martim09/Documents/R/UCI-Bike-Rankings/Data"
 WTT <- c("ALM", "AST", "TBM", "BOH", "CCC", "DQT", "EF1", "GFC",
@@ -17,7 +19,7 @@ filelist <- list.files(path = filepath)
 for (i in 1:length(filelist)) {
   temp_data <- read_excel(paste0(filepath, "/", filelist[i]), range = cell_cols("A:E"))
   temp_data <- temp_data %>% 
-    mutate(Month = i - 1) # We are doing the -1 here because first file (13 Jan) is baseline ranking
+    mutate(Date = dmy(str_sub(filelist[i], -13, -6)))
   raw_tibble <- bind_rows(raw_tibble, temp_data)
 }
 
@@ -25,37 +27,49 @@ for (i in 1:length(filelist)) {
 
 teamrankings <- raw_tibble %>% 
   filter(`Team Code` %in% WTT) %>% 
-  mutate(Date = case_when(
-    Month == 0 ~ "13/1/19",
-    Month == 1 ~ "27/1/19",
-    Month == 2 ~ "24/2/19",
-    Month == 3 ~ "31/3/19",
-    Month == 4 ~ "28/4/19",
-    Month == 5 ~ "26/5/19",
-    Month == 6 ~ "30/6/19",
-    Month == 7 ~ "28/7/19",
-    Month == 8 ~ "25/8/19",
-    Month == 9 ~ "29/9/19",
-    Month == 10 ~ "18/10/19",
-  ),
-  Date = dmy(Date)) 
-
-baselinePoints <- teamrankings %>% 
-  filter(Month == 0)
+  arrange(Date, Rank)
 
 # Plot
 
-teamrankings %>% 
-  filter(Rank <= 5) %>% 
-  ggplot(aes(x = Date, y = Points, col = Name)) +
-  geom_point() +
-  scale_color_viridis_d() +
-  transition_time(Date) + 
-  labs(title = "Date: {frame_time}") +
-  shadow_wake(wake_length = 0.1, alpha = FALSE) +
-  shadow_mark(alpha = 0.3, size = 0.5)
+teamColours <- c("#35E1EE","#009F7D", "#214699", "#16A5CF", "#951433", "#FEB729")
+Top3Teams <- c("AST", "DQT", "INS", "BOH", "MOV", "TJV")
 
-# In-conding checks
+top3anim <- teamrankings %>%
+  filter(`Team Code` %in% Top3Teams) %>%
+  mutate(AlphaLine = ifelse(Rank > 3, 0.7, 0.9),
+         AlphaPoint = ifelse(Rank > 3, 0.7, 1),
+         AlphaText = ifelse(Rank > 3, 0.7, 1)) %>%
+  ggplot(aes(x = Date, y = Points, col = Name, label = paste0(`Team Code`, ": #", Rank))) +
+  geom_line(size = 2.5, aes(alpha = AlphaLine, group = Name)) +
+  geom_point(size = 3, aes(alpha = AlphaPoint), show.legend = F) +
+  geom_text(hjust = 0, nudge_x = 3, aes(alpha = AlphaText), show.legend = F) +
+  scale_colour_manual(values = teamColours) +
+  theme_minimal() +
+  theme(legend.position = "bottom",
+        plot.title = element_text(size = 15, face = "bold"),
+        axis.title.y = element_text(size = 15),
+        axis.title.x = element_text(size = 15),
+        axis.text = element_text(size = 12),
+        legend.title = element_text(size = 15),
+        legend.text = element_text(size = 12)) +
+  guides(alpha = FALSE) +
+  scale_x_date(limits = as.Date(c("2019-01-01","2019-11-15"))) +
+  transition_reveal(Date) +
+  labs(y = "Points",
+       x = "Date",
+       col = "Teams",
+       title = "UCI Team Ranking Top 3 (Road) - Season 2019",
+       subtitle = "Date: {frame_along}",
+       caption = "Created by @JohnnyZeeGerman; www.marmoe.xyz")
+
+top3anim <- animate(top3anim, nframes = 100, fps = 10, type = "cairo", res = 80, width = 800, height = 450)
+
+anim_save("top3anim.gif", top3anim)
+
+# Table
 
 teamrankings %>% 
-  filter(`Team Code` == "INS")
+  filter(Rank <= 3) %>% 
+  group_by(Date, Name, Rank) %>% 
+  summarise() %>% 
+  View()
